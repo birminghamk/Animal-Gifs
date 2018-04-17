78c6e74a31c100a184681ffb41e758978d468091
Kate Birmingham <kabi6686@colorado.edu>
2018-04-16 18:28:56 -0600

    changed styles to buttons/container/background img, added favicon

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,6 @@
-//On Page load: 
-
-$ (function() {
-	// set variables:
-		// buttonTitles = ["baseball", "hockey", "football"];
-		var buttonTitles = ["snake", "owl", "tiger"];
-		// displayButtons - function
+$(function() {
+		var buttonTitles = ["snake", "owl", "lizard"];
+		
 		function displayButton () {
 		// Clear previous buttons from screen
 		$("#buttons").val("");
@@ -24,7 +20,7 @@ $ (function() {
 
 		displayButton();
 
-		//when the user clicks on one of the buttons - function
+		// click on button, display images/ratings
 		$(document).on("click", ".individualButton", function(event) {
 			// prevent default
 			event.preventDefault();
@@ -34,14 +30,12 @@ $ (function() {
 			var rating = "";
 			//clear out old images from page (.empty)
 			$("#imageswithDescription").empty();
-			// AJAX call to GIPHY
-			//set queryURL with designated variables
+	
 			var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating= " + rating + "&limit=10&api_key=IZ1X641xsZu5eeKFKlzEF6TSwOBQnrGZ"
 
+			// AJAX call to GIPHY
 			$.ajax({
-				// URL | //https:
 				url: queryURL,
-				// Method | GET
 				method: "GET"
 
 			}).done(function(response) {
@@ -51,13 +45,10 @@ $ (function() {
 				var results = (response.data);
 
 				for (var i = 0; i < results.length; i++) {
-					// create jQuery div
 					var div = $("<div>")
-					//add class to div
 					div.addClass("images");
-					// create jQuery image
 					var img = $("<img>")
-					// set the src attribute of jQuery image to be the image from GIPHY response (data[i].images.original_still)
+					// set the src attribute of jQuery image to be the image from GIPHY
 					img.attr("src", results[i].images.original_still.url);
 					//set the resting data state to non-animated
 					img.attr("data-state", "still")
@@ -67,28 +58,20 @@ $ (function() {
 					img.attr("data-animate", results[i].images.original.url);
 					//set data-still attribute to still URL
 					img.attr("data-still", results[i].images.original_still.url)
-					// create jQuery paragraph
 					var p = $("<p>");
-					// put the rating from GIPHY response into paragraph created (response.data[i].rating)
 					p.html("Rating: " + results[i].rating);
-					//append jQuery image to jQuery div
 					div.append(img);
-					//append jQuery paragraph to jQuery div
 					div.append(p);
 					//append jQuery div to the page
 					$("#imageswithDescription").append(div);
 					
 				}
+			}); 
+		}) 
 
-			}); //END AJAX CALL
-
-		}) // END BUTTON CLICK FUNCTION
-
-		// on click of form submit button - function
 		$("#formSubmit").on("click", function() {
-			//create variable for userText that get text from input field
-			var userText = $("#exampleInputAnimal1").val();
-			//create jQuery button
+			//create variable for userText that gets text from input field
+			var userText = $("#userText").val();
 			var button = $("<button>");
 			//clear previous buttons from the page
 			$("#buttons").empty();
@@ -98,14 +81,11 @@ $ (function() {
 			button.addClass("individualButton");
 			//insert text from input field to button html
 			button.text(userText);
-			//append each button to the #buttons div
 			$("#buttons").append(button);
-			// run displayButtons function
 			displayButton();
 		
-		}); // END FORM SUBMIT BUTTON
+		}); 
 
-		//on click of image div - function
 		$(document).on("click", "img", function() {
 			//create variable for still/animated conditions
 			var state = $(this).attr("data-state");
@@ -125,16 +105,14 @@ $ (function() {
       
        		}
 
-       		      //it's already animated
+       		     //it's already animated
             	 //reset image src to still version
             	 $(this).attr("src", $(this).attr("data-"+newState));
             	 // change state variable to still 
             	 $(this).attr("data-state", newState);
 
-		})// END IMAGE CLICK FUNCTION
-					
-			
+		})		
 
-}) // END READY FUNCTION
+}) 
 
 
